f8453b054d90bde5c5af882c9f128fc4dc53453d
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2016-12-19 18:14:33 +0300

    clean

--- md_sim/lj.h
+++
@@ -1,9 +0,0 @@
-#ifndef LJ_H
-#define LJ_H
-
-#include "prtcl.h"
-
-double lj_U(struct prtcl_t *p1, struct prtcl_t *p2, double size);
-struct vector3_t *lj_F(struct vector3_t *F, struct prtcl_t *p1, struct prtcl_t *p2, double size);
-
-#endif /* LJ_H */
